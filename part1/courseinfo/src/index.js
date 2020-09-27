cfeb921f0df2b0b89c2fab82ee8c7920cde8530d
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-09-27 21:43:47 +0300

    1.2 step2

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -45,7 +45,7 @@ const App = () => {
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
-  const exercises2 = 7
+  const exercises2 = 7 
   const part3 = 'State of a component'
   const exercises3 = 14
 
