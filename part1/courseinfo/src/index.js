de3761637d218d51ac1fd85cac6dabc6d99df817
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-09-28 14:08:23 +0300

    h1.3 step3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-console.log('blaaä');
+console.log('blaa');
 
 const Header = (props) => {
+  console.log('header props', props)
   return (
     <div>
       <h1>
@@ -13,51 +14,78 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log('part props ', props)
+  const name = props.name
+  const ne = props.ne
+  console.log('part ne ', ne)
   return (
     <div>
       <p>
-        {props.subject} {props.ne}
+        {name} {ne}
       </p>
     </div>
   )
 }
 
 const Content = (props) => {
+  console.log('Content props ', props)
+  const part1 = props.part1
+  console.log('Content props part1 ', props.part1)
+  const part2 = props.part2
+  const part3 = props.part3
   return (
     <div>
-       < Part subject = {props.line1.subject} ne = {props.line1.ne}/>
-       < Part subject = {props.line2.subject} ne = {props.line2.ne}/>
-       < Part subject = {props.line3.subject} ne = {props.line3.ne}/>
+       < Part name = {part1.name} ne = {part1.ne}/>
+       < Part name = {part2.name} ne = {part2.ne}/>
+       < Part name = {part3.name} ne = {part3.ne}/>
     </div>
   )
 }
 
 function Total(props) {
+  console.log('Total props', props)
+  const ne1 = props.part1.ne  
+  const ne2 = props.part2.ne
+  const ne3 = props.part3.ne
+  const totalE = ne1 + ne2 + ne3
+  console.log('Total ne1', ne1)
+  console.log('TotalE ', totalE)
   return (
     <div>
-      <p>Number of exercises {props.ne1 + props.ne2 + props.ne3}</p>
+      <p>Number of exercises {totalE} </p>
     </div>
   );
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7 
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React', 
+    ne: 10,
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    ne: 7 ,
+  }
+  const part3 = {
+    name: 'State of a component',
+    ne: 14,
+  }
 
   return (
     <div>
       <Header course = {course}/>
       <Content 
-        line1 = {{subject: part1, ne: exercises1}} 
-        line2 = {{subject: part2, ne: exercises2}} 
-        line3 = {{subject: part3, ne: exercises3}} 
+      part1 = {part1}
+      part2 = {part2}
+      part3 = {part3}
+
       />    
-      <Total ne1 = {exercises1} ne2 = {exercises2} ne3 = {exercises3} />
+      <Total         
+      part1 = {part1}
+      part2 = {part2}
+      part3 = {part3}
+      />
     </div>
   )
 }
