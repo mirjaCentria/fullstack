188c840e6b6f48fc2416e432f41ed38b7a7208da
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-10-14 03:23:55 +0300

    1.7 unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,10 +6,39 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const sumAll = () => {    
+    return good + neutral + bad
+  }
+
+  const average = () => {
+    if(sumAll() === 0) return 0
+    else return (good - bad) /sumAll()
+  }
+
+  const percent = () => {
+    if(sumAll() === 0) return 0
+    else return 100*good/sumAll()
+  }
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1> 
+      <p>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>  
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      </p>
+      
+      <h1>statistics</h1>  
+      
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {sumAll()}</p>
+      <p>average {average()}</p>
+      <p>positive {percent()} %</p>
+
+
     </div>
   )
 }
