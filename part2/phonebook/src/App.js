483fab7afadf96d1b436d260c3ce3eaeca78f752
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-11-22 01:04:50 +0200

    3.10 && 3.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -36,6 +36,8 @@ const App = () => {
     {
         window.alert('$newName is already added to phonebook') 
         console.log('button clicked alert', event.target)
+        setNewName('')
+        setNewNumber('')
     }else
     {
         console.log('Add', newName)
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+//const baseUrl = 'http://localhost:3001/api/persons'
 //const baseUrl = 'https://phonebookmirja.herokuapp.com/api/persons' 
 //const baseUrl = 'http://localhost:3001/api/persons'
-//const baseUrl = '/api/persons'
+const baseUrl = '/api/persons'
 
 
 const getAll = () => {
