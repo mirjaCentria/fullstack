1d96b7e2767813b4b0d394ef50b8b2072eada1fa
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-12-07 00:32:34 +0200

    4.2

---
+++ part4/App.js
@@ -0,0 +1,24 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.info('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
