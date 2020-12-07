26ff79cae313456308f45040eb80e6776f398f83
Mirja Uusitalo <mirja.uusitalo@centria.fi>
2020-12-07 07:34:37 +0200

    4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -41,6 +41,34 @@ beforeEach(async () => {
     const response = await api.get('/api/blogs')
   })
   
+  test('unique identifier named id', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+  })
+
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+        title: 'valid blog',
+        author: "vb",
+        url: "www.validblog.com",
+        likes: 22
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogs = await Blog.find({})     
+    expect(blogs).toHaveLength(testBlogs.length + 1)
+    
+    const authors = blogs.map(aut => aut.author)  
+    expect(authors).toContain(
+      newBlog.author
+    )
+  })
+
   afterAll(() => {
     mongoose.connection.close()
 })
