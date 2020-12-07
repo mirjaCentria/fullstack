8b70496096f232c82144ba76f632d35d8feea04b
Mirja Uusitalo <mirja.uusitalo@centria.fi>
2020-12-07 08:21:28 +0200

    4.13

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -49,7 +49,7 @@ beforeEach(async () => {
   test('a valid blog can be added', async () => {
     const newBlog = {
         title: 'valid blog',
-        author: "vb",
+        author: "Val Blogger",
         url: "www.validblog.com",
         likes: 22
     }
@@ -69,6 +69,59 @@ beforeEach(async () => {
     )
   })
 
+  test('if no likes, default to 0', async () => {
+    const newBlog = {
+        title: 'No Likey',
+        author: "Bad Author",
+        url: "www.unlikeable.com",
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+   const blogs = await Blog.find({})
+
+   expect(blogs[testBlogs.length].likes).toBe(0)
+})
+
+test('blog needs title and url', async () => {
+  const newBlog = {
+      author: "author"
+  }
+
+  await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+  const blogs = await Blog.find({})
+
+  expect(blogs).toHaveLength(testBlogs.length)
+})
+
+test('succeeds with status code 204 if id is valid', async () => {
+  const blogs = await Blog.find({})
+  const blogToDelete = blogs[0]
+
+  await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+  const blogsAtEnd = await Blog.find({})  
+  
+  expect(blogsAtEnd).toHaveLength(
+    testBlogs.length - 1
+)
+
+  const blog = blogsAtEnd.map(b => b.title)
+
+  expect(blog).not.toContain(blogToDelete.title)
+})
+
   afterAll(() => {
     mongoose.connection.close()
 })
+
