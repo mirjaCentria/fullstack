cac862000970ca95927d7dbe8db756977db8d38d
Mirja Uusitalo <mirja.uusitalo@fougait.com>
2020-12-07 06:50:29 +0200

    4.7

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const testBlogs = [
+    {
+        title: 'BlaaBlaa',
+        author: "Bertil Bertelsson",
+        url: "www.fullstackopen.com",
+        likes: 35
+    },
+   
+]
+
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+  
+    let blogObject = new Blog(testBlogs[0])
+    await blogObject.save()
+  
+    blogObject = new Blog(testBlogs[1])
+    await blogObject.save()
+  })
+
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+  })
+  
+  afterAll(() => {
+    mongoose.connection.close()
+})
