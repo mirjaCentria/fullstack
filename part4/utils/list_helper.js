7fb09cf7d9617f679f78875f9678d8cfac41751e
Mirja Uusitalo <mirja.uusitalo@centria.fi>
2020-12-07 08:23:19 +0200

    4.13

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,4 @@
+const lodash = ('lodash')
 const dummy = (blogs) => {
     // ...
     return 1
@@ -24,6 +25,35 @@ const favoriteBlog = (blogs) => {
   return fav
 }
 
+const mostBlogs = (blogs) => {
+  const groupedByAuthor = lodash.groupBy(blogs, blog => blog.author)
+  const authorlist = []
+  lodash.forEach(groupedByAuthor, (authorBlogs, author) => {
+      authorlist.push({
+          author: author,
+          blogs: authorBlogs.length
+      })
+  })
+  const sortedAuthorList = lodash.sortBy(authorlist, author => author.blogs)
+  return sortedAuthorList.pop()
+}
+
+const mostLikes = (blogs) => {
+  const groupedByAuthor = lodash.groupBy(blogs, blog => blog.author)
+  const authorList = []
+  lodash.forEach(groupedByAuthor, (authorBlogs, author) => {
+      authorList.push({
+          author: author,
+          likes: totalLikes(authorBlogs)
+      })
+  })
+
+  const sortedAuthorList = lodash.sortBy(authorList, author => author.likes)
+  return sortedAuthorList.pop()
+}
+
   module.exports = {
      dummy, totalLikes, favoriteBlog
   }
+
+  
--- part4/utils/logger.js
+++ part4/utils/logger.js
@@ -1,13 +1,16 @@
   
 const info = (...params) => {
-    console.log(...params)
-  }
-  
-  const error = (...params) => {
+  if (process.env.NODE_ENV !== 'test') {
     console.error(...params)
   }
+}
   
-  module.exports = {
-    info, error
+const error = (...params) => {
+  if (process.env.NODE_ENV !== 'test') {
+     console.error(...params)
   }
+}
   
+module.exports = {
+  info, error
+}
